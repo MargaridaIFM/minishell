d128ed285b74a49f1dbffd7de5c632f43105c054
mfrancis <margarida.matiass@gmail.com>
2025-01-07 10:51:10 +0000

    fixing expander in progresss

--- miniheaven/sources/parser/expander_vars.c
+++ miniheaven/sources/parser/expander_vars.c
@@ -3,33 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   expander_vars.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mistery576 <mistery576@student.42.fr>      +#+  +:+       +#+        */
+/*   By: mfrancis <mfrancis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 21:12:49 by mistery576        #+#    #+#             */
-/*   Updated: 2024/12/07 22:07:36 by mistery576       ###   ########.fr       */
+/*   Updated: 2025/01/07 10:49:46 by mfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-/**
- * @brief Handles single and double quotes during variable expansion.
- * 
- * Single quotes are removed if not inside a double-quote block.
- *  Double quotes are removed and toggle the `dq` flag in the `expander`.
- * 
- * @param minishell Pointer to the `t_minishell` structure/
- * @param token Pointer to the token being processed.
- * @param idx Pointer to the current index in the token string.
- * 
- * @return Void. 
- */
 static void	expand_vars_utils1(t_minishell *minishell, t_token *token, int *idx)
 {
 	if (token->str[*idx] == '\'')
 	{
+		printf("Index 5: %d\n", *idx);
 		if (!minishell->expander->dq)
-			rm_single_quotes(token, minishell, idx);
+		{
+			printf("Index 5_1: %d\n", *idx);
+			
+			// rm_one_single_quotes(token, minishell, idx);
+			// printf("Index 5_2: %d\n", *idx);
+			// while(token->str[*idx] != '\'')
+			// 	idx++;
+			// printf("Index 5_3: %d\n", *idx);
+			// rm_one_single_quotes(token, minishell, idx);
+			// printf("Index 5_4: %d\n", *idx);
+
+		}
 	}
 	else if (token->str[*idx] == '"')
 	{
@@ -38,25 +38,12 @@ static void	expand_vars_utils1(t_minishell *minishell, t_token *token, int *idx)
 	}
 }
 
-/**
- * @brief Replaces a variable in the token string with its value.
- * 
- * This function replaces a variable's name in the token string with its value. 
- * It adjusts the string length, frees old memory, and updates the token
- * type if necessary.
- * 
- * @param minishell Pointer to the `t_minishell`.
- * @param token Pointer to the token being processed.
- * @param idx Pointer to the current index in the token string.
- * @param new_str Pointer to hold the newly allocated string after replacement.
- * 
- * @return Void.
- */
 static void	support(t_minishell *minishell,
 	t_token *token, int *idx, char **new_str)
 {
 	if (minishell->expander->dq || !minishell->expander->dq)
 	{
+		printf("Index 2_2_4: %d\n", *idx);
 		*idx = *idx - ft_strlen(minishell->expander->var_name) - 1;
 		*new_str = replace_var_in_str(token->str, minishell, idx);
 		free(token->str);
@@ -67,41 +54,33 @@ static void	support(t_minishell *minishell,
 	}
 }
 
-/**
- * @brief Extracts the variable name and replaces it with its content.
- * 
- * This function identifies a variable in the token string starting with `$`,
- * extracts its name,  * and retrieves its value using `extrat_var_content`.
- * 
- * @param minishell Pointer to the `t_minishell`.
- * @param token Pointer to the token being processed.
- * @param idx Pointer to the current index in the token string.
- * @param new_str Pointer to hold the newly allocated string after replacement.
- * 
- * @return Void.
- * 
- */
 static void	expand_vars_utils2(t_minishell *minishell,
 	t_token *token, int *idx, char **new_str)
 {
 	(*idx)++;
+	printf("Index 2_2_1: %d\n", *idx);
 	if (token->str[*idx] == '?')
 	{
 		minishell->expander->var_name = ft_strdup("?");
 		(*idx)++;
 	}
-	else if (!ft_isalpha(token->str[*idx]) && token->str[*idx] != '_')
+	// else if (token->str[*idx] == '\'')
+	// 	rm_single_quotes(token, minishell, idx);
+	else if (!ft_isalpha(token->str[*idx]) && token->str[*idx] != '_' )
 	{
+		printf("Index 2_2_2: %d\n", *idx);
 		minishell->expander->var_name
 			= add_char(token->str[*idx], minishell->expander->var_name);
 		(*idx)++;
 	}
 	else
 	{
+		printf("Index 2_2_3: %d\n", *idx);
 		while (token->str[*idx]
 			&& (ft_isalnum(token->str[*idx])
 				|| token->str[*idx] == '_'))
 		{
+			printf("Index 2_2_3_1: %d\n", *idx);
 			minishell->expander->var_name
 				= add_char(token->str[*idx], minishell->expander->var_name);
 			(*idx)++;
@@ -110,21 +89,6 @@ static void	expand_vars_utils2(t_minishell *minishell,
 	minishell->expander->var_content = extrat_var_content(minishell);
 	support(minishell, token, idx, new_str);
 }
-/**
- * @brief Expands environment variables and processes quotes in a token string.
- * 
- * This function handles the expansion of environment variables within a token's 
- * string by identifying and replacing variables prefixed with `$`. 
- * Processes single and double quotes to handle their special meaning 
- * Auxiliary functions are used for specific tasks like managing quotes, 
- * extracting variable content, and replacing variables in the string.
- * If a variable is not found, it is replaced with an empty string.
- * 
- * @param minishell Pointer to the `t_minishell` structure.
- * @param token Pointer to the token whose string will be processed and expanded.
- * 
- * @return Void. Modifies the token str.
- */
 
 void	expand_vars(t_minishell *minishell, t_token *token)
 {
@@ -137,18 +101,81 @@ void	expand_vars(t_minishell *minishell, t_token *token)
 	if (!minishell->expander)
 		free_exit(minishell, "Error - Fail allocating expander");
 	reset_expand(minishell->expander);
+	printf("Index 1: %d\n", idx);
 	while (token->str[idx])
 	{
-		if (token->str[idx] == '\'' || token->str[idx] == '"')
+		printf("Index 2: %d\n", idx);
+		printf("str: %c\n", token->str[idx]);
+		if (token->str[idx] == '$' && token->str[idx + 1] == '\'')
+		{ 
+			printf("Index 4: %d\n", idx);
+			printf("str dollar: %s\n", token->str);
+			
+			rm_dollar(token, minishell, &idx);
+			printf("str dollar apos: %s\n", token->str);
+
+			printf("Index 4.1: %d\n", idx);
+
+			rm_one_single_quotes(token, minishell, &idx);
+			printf("Index 4.2: %d\n", idx);
+			while(token->str[idx] != '\'')
+				idx++;
+			printf("Index 4.3: %d\n", idx);
+				
+			rm_one_single_quotes(token, minishell, &idx);
+
+		}
+		if (token->str[idx] == '$' && token->str[idx + 1] == '\"')
+		{
+			rm_dollar(token, minishell, &idx);
+			idx--;
+		}
+		else if (token->str[idx] == '\'' || token->str[idx] == '"')
+		{
+			printf("Index 2_1: %d\n", idx);
 			expand_vars_utils1(minishell, token, &idx);
+			printf("str apos: %s\n", token->str);
+
+		}
 		else if (token->str[idx] == '$' && !token->str[idx + 1]
 			&& !minishell->expander->dq)
 			return ;
 		else if (token->str[idx] == '$')
 		{
+			printf("Index 2_2: %d\n", idx);
 			expand_vars_utils2(minishell, token, &idx, &new_str);
 			free_expand(minishell->expander);
 		}
 		idx++;
 	}
+}
+void rm_dollar(t_token *token, t_minishell *minishell, int *idx)
+{
+	int		i;
+	char	*new_str;
+	int j;
+	
+	printf("aqui\n");
+	(void)minishell;
+	i = 0;
+	new_str = malloc(sizeof(char) * (ft_strlen(token->str)));
+	if (!new_str) {
+        free_exit(minishell, "Error - Fail allocating memory for new str\n");
+    }
+	while(i < *idx)
+	{
+		new_str[i] = token->str[i];
+		i++;
+	}
+	j = i;
+	i++;
+	while (token->str[i])
+	{
+		new_str[j] = token->str[i];
+		j++;
+		i++;
+	}
+	new_str[j] = '\0';
+	free(token->str);
+	token->str = new_str;
 }
